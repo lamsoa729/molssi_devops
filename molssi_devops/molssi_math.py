8bd03ce13d77b2eaebf36829faaa565dff10d9c8
Adam Lamson <adam.lamson@colorado.edu>
2019-05-13 13:47:11 -0600

    Minor changes to setup.cfg for yapf

--- molssi_devops/molssi_math.py
+++ molssi_devops/molssi_math.py
@@ -6,27 +6,47 @@ Handles the primary functions
 """
 
 
+def mean(my_list):
+    """!Calculate the mean of the list.
+
+    @param my_list: list of python objects that can be summed (float, int, etc.)
+    @return: Mean of my_list (float)
+
+    """
+    return sum(my_list) / len(my_list)
+
+
 def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
+    """!Math of the molssi
 
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
+    @param with_attribution: bool, Optional, (default=True)
         Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
+    @return: quote (str)
         Compiled string including quote and optional attribution
-    """
 
+    """
     quote = "The code is but a canvas to our imagination."
     if with_attribution:
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
+
+
+# def canvas(with_attribution=True):
+#     """
+#     Placeholder function to show example docstring (NumPy format)
+
+#     Replace this function and doc string for your own project
+
+#     Parameters
+#     ----------
+#     with_attribution : bool, Optional, default: True
+#         Set whether or not to display who the quote is from
+
+#     Returns
+#     -------
+#     quote : str
+#         Compiled string including quote and optional attribution
+#     """
 
 
 if __name__ == "__main__":
